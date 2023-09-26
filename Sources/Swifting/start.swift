59379d37690b0aff64ea444887011f4a6bbb71d3
Jihoonahn <jihoonahn.dev@gmail.com>
2023-09-26 20:15:57 +0900

    Update Main

---
+++ Sources/Swifting/start.swift
@@ -0,0 +1,14 @@
+import ScadeKit
+
+final class Swifting: SCDApplication {
+
+	let window = SCDLatticeWindow()
+  
+	override func onFinishLaunching() {
+		#if os(iOS)
+		Navigation.show(.splash, in: window)
+		#elseif os(Android)
+		Navigation.show(.home, in: window)
+		#endif
+	}
+}
