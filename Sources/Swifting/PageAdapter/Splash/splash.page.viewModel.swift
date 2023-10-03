01921d97ade70d69611ea7889ec29eabae0027d0
Jihoonahn <jihoonahn.dev@gmail.com>
2023-10-04 00:21:18 +0900

    Update Splash

--- Sources/Swifting/PageAdapter/Splash/splash.page.viewModel.swift
+++ Sources/Swifting/PageAdapter/Splash/splash.page.viewModel.swift
@@ -4,7 +4,7 @@ final class SplashPageViewModel: BaseViewModel {
     func splashAction() {
         #if os(iOS)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            Navigation.go(.home, clearHistrory: false)
+            Navigation.go(.home, clearHistrory: true)
         }
         #endif
     }
